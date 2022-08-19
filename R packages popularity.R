2146991581cf8ef9cc0a46327d9c2956e8df584b
Tony Fung <tony.fung@itech.media>
2022-08-20 00:05:39 +0100

    fixed mechanism responsible for downloading only files we don't have

--- R packages popularity.R
+++ R packages popularity.R
@@ -7,100 +7,100 @@ library(data.table)
 setDTthreads(threads = parallel::detectCores()-1)
 
 
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#                   1. download all log files (to RAM) ----
-#_______________________________________________________________________________
-
-# construct URLs
-x <- Sys.Date() - 372                          # start
-y <- Sys.Date() - 7                            # end
-z <- seq(x, y, by = 'day')                     # all dates
-a <- as.POSIXlt(z)$year + 1900                 # year
-b <- paste0('http://cran-logs.rstudio.com/', a, '/', z, '.csv.gz')   # URLs
-c <- c('date', 'time', 'package', 'country')   # download specific columns ONLY
-
-# # download csvs (serially)
-# system.time(
-# d <- lapply(b, \(i) fread(i, select = c))
-# )
-
-
-# setup computing cluster (parallel running)
-n_cores <- detectCores() - 1
-cl <- makeCluster(n_cores)
-registerDoParallel(cl, cores = n_cores)
-
-# download csvs (parallel running)
-system.time(
-            d <- foreach(i = seq_along(b)
-                         , .packages = c("data.table")
-                         ) %dopar%
-              {
-                fread(b[[i]], select = c)
-              }
-            )
-
-# stop hoarding cluster
-stopCluster(cl)
-
-# clean environment
-rm(list = setdiff(letters[1:26], 'd')
-   , n_cores, cl
-   )
-
-
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#                       2. rbind csvs (RAM method)  ----
-#_______________________________________________________________________________
-
-# rbind
-system.time( df <- rbindlist(d) )
-rm(d)
-
-# save as .RData
-system.time( save(df, file = "CRANlogs/CRANlogs.RData") )
-
-# for later analyses: load the saved data.table
-# load("CRANlogs/CRANlogs.RData")
-
-# define variable types
-df[, `:=` (#date = parse_date_time2(date, "%Y-%m-%d")
-           # , package = factor(package)
-           # , country = factor(country)
-            day_of_week = lubridate::wday(date, week_start = 1)  # 1 = Monday
-           , week = lubridate::isoweek(date)                     # week starts on Monday
-           , hour = substr(time, 1,2)
-           )
-   ]
-
-
-
-
 # #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# #                   1. download all log files (to HDD) ----
+# #                   1. download all log files (to RAM) ----
 # #_______________________________________________________________________________
 # 
 # # construct URLs
-# x <- Sys.Date() - 365                     # start
-# y <- Sys.Date() - 1                       # end
-# z <- seq(x, y, by = 'day')                # all dates
-# a <- as.POSIXlt(z)$year + 1900            # year
-# b <- paste0('http://cran-logs.rstudio.com/', a, '/', z, '.csv.gz')   # urls
+# x <- Sys.Date() - 372                          # start
+# y <- Sys.Date() - 7                            # end
+# z <- seq(x, y, by = 'day')                     # all dates
+# a <- as.POSIXlt(z)$year + 1900                 # year
+# b <- paste0('http://cran-logs.rstudio.com/', a, '/', z, '.csv.gz')   # URLs
+# c <- c('date', 'time', 'package', 'country')   # download specific columns ONLY
 # 
-# # only download the files you don't have:
-# dir.create("CRANlogs")
-# c <- setdiff(as.character(z), tools::file_path_sans_ext(dir("CRANlogs"), T))
-# 
-# # start downloads
-# xxx <- Sys.time()
-# for (i in seq_along(c))
-# {
-#   print(paste0(i, "/", length(c)))
-#   download.file(b[i], paste0('CRANlogs/', c[i], '.csv.gz'))
-# }
-# print(paste('download took', round(difftime(Sys.time(), xxx, units = 'mins'), 2), 'mins'))
+# # # download csvs (serially)
+# # system.time(
+# # d <- lapply(b, \(i) fread(i, select = c))
+# # )
 # 
 # 
+# # setup computing cluster (parallel running)
+# n_cores <- detectCores() - 1
+# cl <- makeCluster(n_cores)
+# registerDoParallel(cl, cores = n_cores)
+# 
+# # download csvs (parallel running)
+# system.time(
+#             d <- foreach(i = seq_along(b)
+#                          , .packages = c("data.table")
+#                          ) %dopar%
+#               {
+#                 fread(b[[i]], select = c)
+#               }
+#             )
+# 
+# # stop hoarding cluster
+# stopCluster(cl)
+# 
+# # clean environment
+# rm(list = setdiff(letters[1:26], 'd')
+#    , n_cores, cl
+#    )
+# 
+# 
+# #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# #                       2. rbind csvs (RAM method)  ----
+# #_______________________________________________________________________________
+# 
+# # rbind
+# system.time( df <- rbindlist(d) )
+# rm(d)
+# 
+# # save as .RData
+# system.time( save(df, file = "CRANlogs/CRANlogs.RData") )
+# 
+# # for later analyses: load the saved data.table
+# # load("CRANlogs/CRANlogs.RData")
+# 
+# # define variable types
+# df[, `:=` (#date = parse_date_time2(date, "%Y-%m-%d")
+#            # , package = factor(package)
+#            # , country = factor(country)
+#             day_of_week = lubridate::wday(date, week_start = 1)  # 1 = Monday
+#            , week = lubridate::isoweek(date)                     # week starts on Monday
+#            , hour = substr(time, 1,2)
+#            )
+#    ]
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#                   1. download all log files (to HDD) ----
+#_______________________________________________________________________________
+
+# construct URLs
+x <- Sys.Date() - 365                     # start
+y <- Sys.Date() - 1                       # end
+z <- seq(x, y, by = 'day')                # all dates
+a <- as.POSIXlt(z)$year + 1900            # year
+b <- paste0('http://cran-logs.rstudio.com/', a, '/', z, '.csv.gz')   # urls
+
+# only download the files you don't have:
+dir.create("CRANlogs")
+z <- setdiff(as.character(z), tools::file_path_sans_ext(dir("CRANlogs"), T))
+a <- as.POSIXlt(z)$year + 1900
+b <- paste0('http://cran-logs.rstudio.com/', a, '/', z, '.csv.gz')   # urls
+
+# start downloads
+xxx <- Sys.time()
+for (i in seq_along(b))
+{
+  print(paste0(i, "/", length(b), ' or ', round(i/length(b)*100, 1), ' %'))
+  download.file(b[i], paste0('CRANlogs/', z[i], '.csv.gz'))
+}
+print(paste('download took', round(difftime(Sys.time(), xxx, units = 'mins'), 2), 'mins'))
+
+
 # #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # #                          2. import csvs (from HDD) ----
 # #_______________________________________________________________________________
